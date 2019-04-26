9e06e7c772e15376fe732860e702e10084cec0df
Juninho Silva <jsdeveloperweb@gmail.com>
2019-04-26 19:09:39 -0300

    configurando o mongoose

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const databaeConfig = require('./config/database')
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+
+    this.database()
+    this.middlewares()
+    this.routes()
+  }
+  database () {
+    mongoose.connect(databaeConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
+  }
+  middlewares () {
+    this.express.use(express.json())
+  }
+  routes () {
+    this.express.use(require('./routes'))
+  }
+}
+module.exports = new App().express
