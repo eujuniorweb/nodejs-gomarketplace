f3eb46fc6acfe16df5ed0fabc5506e84a96d34ec
Juninho Silva <jsdeveloperweb@gmail.com>
2019-04-27 03:02:56 -0300

    schema de ad

---
+++ src/app/models/Ad.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const auth = require('../../config/auth')
+
+const AdSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Ad', AdSchema)
